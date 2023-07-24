9cbd811d294fb5a20ca50cefaa78f31cc19b0be5
suraj waykar <surajwaykar24@gmail.com>
2023-07-25 00:13:10 +0530

    suraj deleted the file

--- Hello.java
+++ Hello.java
@@ -1,8 +1,7 @@
 public class Hello{
     public static void main(String[] args) {
         
-        System.out.println("git practice");
-        System.out.println("branch practice");
+        
         
     }
 }
